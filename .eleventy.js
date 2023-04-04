5f655c8ca0bc3773ce70cacb35b2c4c7fc21086b
David Pierce <davidaaronpierce@gmail.com>
2023-04-04 16:56:26 +0000

    Changes for gh-pages deploy

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,17 @@
 module.exports = function (eleventyConfig) {
+  // Add filters to Nunjucks
+  eleventyConfig.addFilter("dateDisplay", require("./src/_filters/dates.js"));
 
-	// Add filters to Nunjucks
-	eleventyConfig.addFilter("dateDisplay", require("./src/_filters/dates.js") );
-	
-	eleventyConfig.addPassthroughCopy("./src/css/")
-	eleventyConfig.addPassthroughCopy("./src/images/")
-	eleventyConfig.addWatchTarget("./src/css/")
-	eleventyConfig.addWatchTarget("./src/images/")
+  eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addWatchTarget("./src/images/");
 
-	return {
-		dir: {
-			input: "src",
-			output: "public"
-		},
-		pathPrefix: "/eleventy-bragdoc/"
-	}
-}
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+    pathPrefix: "",
+  };
+};
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# eleventy-bragdoc
+# bragdoc
 
 It's my personal bragdoc, cloned from [emilyyleung/eleventy-bragdoc](https://github.com/emilyyleung/eleventy-bragdoc). Go there to start making your own!
 
 ## Todo
 
-- [ ] Change `data.json` to me
+- [x] Change `data.json` to me
 - [ ] Put my stuff on here
 - [ ] Style it more like my personal website
